62b0b7114ccf64138790ba2c2c520999c2a46ff3
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-10 16:58:01 +0200

    getAllpost in controller whithout populate

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -22,4 +22,20 @@ const addPost = async (req, res) => {
   }
 };
 
-module.exports = { addPost };
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    return res.status(200).json({
+      ok: true,
+      posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: `Please contact the administrator ${error.message}`,
+    });
+  }
+};
+
+module.exports = { addPost, getAllPosts };
