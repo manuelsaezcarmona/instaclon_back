cb0cbfbd59dfcbd5ba0bbd0ff19866d0257c145d
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-10 14:18:56 +0200

    add authorizeToken middleware

--- helpers/auth.helper.js
+++ helpers/auth.helper.js
@@ -8,4 +8,23 @@ const createJWT = (id, username) => {
   return jwToken;
 };
 
-module.exports = { createJWT };
+const authorizeToken = (req, res, next) => {
+  const authorization = req.header('Authorization');
+  let token = '';
+
+  if (!authorization) {
+    res.status(401).send({ message: 'Access denied' });
+  }
+  try {
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+      token = authorization.substring(7);
+      const verified = jwt.verify(token, process.env.SECRET_JWT_SEED);
+      console.log(verified);
+      req.user = verified;
+    }
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid Token');
+  }
+};
+module.exports = { createJWT, authorizeToken };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 const usersRouter = require('express').Router();
 const { addUser, getUserById } = require('../controllers/user.controller');
+const { authorizeToken } = require('../helpers/auth.helper');
 
 usersRouter.post('/new', addUser);
-usersRouter.get('/:id', getUserById);
+usersRouter.get('/id', authorizeToken, getUserById);
 
 module.exports = usersRouter;
