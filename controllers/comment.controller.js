68d8a78d897f4b62faa5406309429d12e2050732
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-11 11:45:11 +0200

    deelteComentToPostAndUser working in comment.controller.js

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -44,11 +44,73 @@ const addComment = async (req, res) => {
       msg: `Please contact the administrator ${error.message}`,
     });
   }
-
-  console.log(comment);
-  return res.json({
-    comment,
-  });
 };
 
-module.exports = { addComment };
+const deleteCommentToPostAndUser = async (postID, userID, commentID) => {
+  try {
+    const user = await User.findById(userID);
+    const userCommentsFiltered = user.comments
+      .map((comment) => comment.toString())
+      .filter((id) => id !== commentID);
+    user.comments = userCommentsFiltered;
+    await user.save();
+
+    const post = await Post.findById(postID);
+
+    const postCommentsFiltered = post.comments
+      .map((comment) => comment.toString())
+      .filter((id) => id !== commentID);
+
+    post.comments = postCommentsFiltered;
+    await post.save();
+  } catch (error) {
+    return error.message;
+  }
+};
+
+const deleteComment = async (req, res) => {
+  const { id } = req.user;
+  const { commentid } = req.params;
+
+  try {
+    const comment = await Comment.findById(commentid);
+
+    const post = await Post.findById(comment.postIDcomment);
+
+    if (!comment) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'this comment does not exist',
+      });
+    }
+
+    if (
+      comment.userIDcomment.toString() !== id ||
+      post.userID.toString() !== id
+    ) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'you do not have permission to delete this post',
+      });
+    }
+
+    const commentDeleted = await Comment.findByIdAndDelete(commentid);
+
+    await deleteCommentToPostAndUser(
+      commentDeleted.postIDcomment,
+      id,
+      commentid
+    );
+    return res.status(201).json({
+      ok: true,
+      msg: 'delete comment',
+      commentDeleted,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: `Please contact the administrator ${error.message}`,
+    });
+  }
+};
+module.exports = { addComment, deleteComment };
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,8 +1,12 @@
 const commentRouter = require('express').Router();
 
 const { authorizeToken } = require('../helpers/auth.helper');
-const { addComment } = require('../controllers/comment.controller');
+const {
+  addComment,
+  deleteComment,
+} = require('../controllers/comment.controller');
 
 commentRouter.post('/add', authorizeToken, addComment);
+commentRouter.delete('/delete/:commentid', authorizeToken, deleteComment);
 
 module.exports = commentRouter;
